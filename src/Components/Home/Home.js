8d0fe27c6118aee11738cc27240e9397882c7634
H.A.M <88446691+HAM-Developer@users.noreply.github.com>
2023-01-17 08:13:18 -0800

    Movie-App

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,22 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { motion } from "framer-motion"
 import { BsSearch } from 'react-icons/bs'
 import Movie from '../Movie/Movie'
+import { MovieContext } from "../../App"
 import "./Home.css"
 import { API_KEY } from '../../API'
 import axios from 'axios'
+import MovieCard from '../Movie/MovieCard'
 function Section() {
     const [feed, setFeed] = useState([])
-    const [upComing, setUpComing] = useState([])
+    const [topRatedTV, setTopRatedTV] = useState([])
+    const { getSeries } = useContext(MovieContext)
+    const [nowPlaying, setNowPlaying] = useState([])
     const searchFeed = (e) => {
-        setUpComing(upComing.filter(movie => e.target.value === movie.title))
+        setTopRatedTV(topRatedTV.filter(movie => e.target.value === movie.title))
         setFeed(feed.filter(movie => e.target.value === movie.title[0]))
-        upComing.forEach(movie => console.log(movie.title[0]))
+        topRatedTV.forEach(movie => console.log(movie.title[0]))
         console.log(e.target.value)
     }
     const getData = () => {
         axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US`).then((response) => setFeed(response.data.results))
-        axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US`).then((response) => setUpComing(response.data.results))
+        axios.get(`https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}&language=en-US`).then((response) => setTopRatedTV(response.data.results))
+        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US`).then((response) => setNowPlaying(response.data.results))
     }
     useEffect(() => {
         getData()
@@ -24,16 +29,23 @@ function Section() {
     return (
         <div className='section'>
             <div className='movie__section'>
-                <div className='search' >
+                {/* <div className='search' >
                     <BsSearch className='searchIcon' />
                     <input type="text" placeholder="Search for movies and TV series" onChange={searchFeed} />
+                </div> */}
+                <div className='heroImages ' >
+                    {
+                        nowPlaying.slice(0, 4).map(dataset => (
+                            <MovieCard key={dataset.id} id={dataset.id} movieTitle={dataset.title} movieImage={dataset.poster_path} movieDesc={dataset.overview} movieGenre={dataset.genre_ids} />
+                        ))
+                    }
                 </div>
                 <h1>Upcoming Movies</h1>
                 <div className='upcoming_movies ' >
                     <motion.div className='movies_wrapper' drag='x' dragConstraints={{ left: -2800, right: 0 }} dragElastic="0.1" dragMomentum={false}  >
                         {
-                            upComing.map(dataset => (
-                                <Movie key={dataset.id} id={dataset.id} movieTitle={dataset.title} movieImage={dataset.poster_path} movieRating={dataset.vote_average} movieYear={dataset.release_date} movieGenre={dataset.genre_ids} />
+                            topRatedTV.map(dataset => (
+                                <Movie key={dataset.id} id={dataset.id} seriesTitle={dataset.name} movieImage={dataset.poster_path} movieRating={dataset.vote_average} seriesYear={dataset.first_air_date} getSeries={getSeries} />
                             ))
                         }
                     </motion.div>
--- src/Components/Movie/Movie.js
+++ src/Components/Movie/Movie.js
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react'
 import "./Movie.css"
 import { BookmarkIcon } from '@heroicons/react/24/outline'
 
-function Movie({ id, movieImage, movieYear, movieTitle, movieRating }) {
+function Movie({ id, movieImage, movieYear, movieTitle, movieRating, seriesYear, seriesTitle }) {
     const [time, setTime] = useState('days')
     const [days, setDays] = useState(null)
+    const [seriesDays, setSeriesDays] = useState(null)
     const movieDate = () => {
         setDays(Math.round(((new Date) - (new Date(`${movieYear}`))) / (1000 * 60 * 60 * 24)))
+        setSeriesDays(Math.round(((new Date) - (new Date(`${seriesYear}`))) / (1000 * 60 * 60 * 24)))
     }
     useEffect(() => {
         movieDate()
@@ -18,11 +20,11 @@ function Movie({ id, movieImage, movieYear, movieTitle, movieRating }) {
             <img className='movie_img' src={`https://image.tmdb.org/t/p/w500${movieImage}`} alt="movie" />
             <div className='movie__info'>
                 <div className='movie__wrapper'>
-                    <p className='movie__year'>{`${days} ${time} ago`} </p>
+                    <p className='movie__year'>{days ? `${days} ${time} ago` : `${seriesDays} ${time} ago`} </p>
                     <p className='movie__rating'>{`${movieRating}`.slice(0, 3)}</p>
                 </div>
                 <li className="movie_link">
-                    <Link to={`/moviedetail/${id}`} className='movie_title' >{movieTitle}</Link>
+                    <Link to={`/moviedetail/${id}`} className='movie_title' >{movieTitle ? movieTitle : seriesTitle}</Link>
                 </li>
             </div>
             <div className='bookmarkedIcon'><BookmarkIcon /></div>
